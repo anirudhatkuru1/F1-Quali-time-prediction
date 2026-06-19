49c71bef39a3af5574faebb3ec09a4d3dac13ca6
Anirudh Atkuru <anirudh.atkuru@gmail.com>
2026-06-19 12:52:33 +1000

    deploying !

--- app.py
+++ app.py
@@ -517,4 +517,6 @@ def get_race_prediction(team_name, race_name):
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
